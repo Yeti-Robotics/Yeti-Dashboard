eb5b98244ee18ad626466dca16578c1b52a09e9c
boomermath <boomermath13@gmail.com>
2024-04-10 18:37:21 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img src="/yeti.png" className="w-[50px] h-[50px]" />
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger>File</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>
+                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+              </MenubarItem>
+              <MenubarItem>New Window</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Share</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Print</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { NT4Provider } from '@frc-web-components/react';
+
+import "./globals.css"
+
+ReactDOM.createRoot(document.getElementById('app')!).render(
+  <React.StrictMode>
+    <NT4Provider address="localhost">
+      <App />
+    </NT4Provider>
+  </React.StrictMode>
+)
