5e702c685a412e5102be0763a4f5417497ce42a0
Drew Beamer <andrewmbeamer@gmail.com>
2024-07-08 15:20:21 -0400

    fixes maximum update depth error

--- src/app/widgets/components/WidgetChooserAccordion.tsx
+++ src/app/widgets/components/WidgetChooserAccordion.tsx
@@ -1,78 +1,127 @@
-
 import { ChevronRightIcon } from "@heroicons/react/16/solid";
-import React, { useContext } from "react";
-import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-headless-accordion";
-import { DragDropContext, useWidgetDragAndDrop } from "../hooks/useWidgetDragDrop";
+import React from "react";
+import {
+    Accordion,
+    AccordionBody,
+    AccordionHeader,
+    AccordionItem
+} from "react-headless-accordion";
+import { useWidgetDragAndDrop } from "../hooks/useWidgetDragDrop";
+import { WidgetChooserNode } from "./WidgetChooser";
 
-function DatakeyWrapper({ dataKey, children }: { dataKey: string, children: React.ReactNode | React.ReactNode[] }) {
+function DatakeyWrapper({
+    dataKey,
+    children
+}: {
+    dataKey: string;
+    children: React.ReactNode | React.ReactNode[];
+}) {
     const { setDataKey } = useWidgetDragAndDrop();
 
-    return (<div onDragStart={_e => {
-        console.log("Drag start for " + dataKey);
-        setDataKey(dataKey);
-    }}>{children}</div>)
+    return (
+        <div
+            onDragStart={() => {
+                console.log("Drag start for " + dataKey);
+                setDataKey(dataKey);
+            }}
+        >
+            {children}
+        </div>
+    );
 }
 
-function AccordionTitle({ dataKey, title, open }: { title: string, open: boolean, dataKey: string }) {
-
+function AccordionTitle({
+    dataKey,
+    title,
+    open
+}: {
+    title: string;
+    open: boolean;
+    dataKey: string;
+}) {
     return (
         <DatakeyWrapper dataKey={dataKey}>
             <AccordionHeader className="inline-flex items-center">
-                <ChevronRightIcon className={`w-4 h-4 ${open ? "rotate-90" : ""}`} />
+                <ChevronRightIcon
+                    className={`w-4 h-4 ${open ? "rotate-90" : ""}`}
+                />
                 <h1 draggable={true}>{title}</h1>
             </AccordionHeader>
         </DatakeyWrapper>
     );
 }
 
-function createSubAccordion(content: any, lvl: number) {
+function SubWidgetChooserAccordion({
+    entries,
+    indent
+}: {
+    entries: WidgetChooserNode;
+    indent: number;
+}) {
+    if (!(entries.content instanceof Array)) {
+        return (
+            <DatakeyWrapper dataKey={entries.key}>
+                <p
+                    className="flex justify-between"
+                    draggable={true}
+                    style={{ marginLeft: indent + 16 + "px" }}
+                >
+                    <span>{entries.title}</span>{" "}
+                    <span className="truncate overflow-hidden text-gray-700 pr-3">
+                        {typeof entries.content}
+                    </span>
+                </p>
+            </DatakeyWrapper>
+        );
+    }
+
+    const accordionItems: JSX.Element[] = (
+        entries.content as WidgetChooserNode[]
+    ).map(contentItem => {
+        return (
+            <SubWidgetChooserAccordion
+                key={contentItem.key}
+                entries={contentItem}
+                indent={indent + 2}
+            />
+        );
+    });
+
     return (
-        <AccordionBody>
-            {content.sort((a: any, b: any) => {
-                if (a.content instanceof Array) {
-                    return 1;
-                } else if (b.content instanceof Array) {
-                    return -1;
-                }
-            }).map((c: any) => (
-                <div key={c.key} style={{ marginLeft: `${lvl * (c.content instanceof Array ? 11 : 16)}px` }}>
-                    <AccordionItem key={c.key}>
-                        {({ open }: { open: boolean }) => (
-                            c.content instanceof Array ? (
-                                <div key={c.key}>
-                                    <AccordionTitle dataKey={c.key} title={c.title} open={open} />
-                                    {createSubAccordion(c.content, lvl + 1)}
-                                </div>) : (<DatakeyWrapper dataKey={c.key}>
-                                    <p className="flex justify-between" draggable={true}>
-                                        <span>{c.title}</span>{" "}
-                                        <span className="truncate overflow-hidden text-gray-700 pr-3">{typeof c.content}</span>
-                                    </p>
-                                </DatakeyWrapper>)
-                        )}
-                    </AccordionItem>
+        <AccordionItem key={entries.key}>
+            {({ open }: { open: boolean }) => (
+                <div style={{ marginLeft: indent + "px" }}>
+                    <AccordionTitle
+                        dataKey={entries.key}
+                        title={entries.title}
+                        open={open}
+                    />
+                    <AccordionBody>{accordionItems}</AccordionBody>
                 </div>
-            ))
-            }
-        </AccordionBody>
+            )}
+        </AccordionItem>
     );
 }
 
-export function WidgetChooserAccordion({ entries, size }: { entries: any, size: number }) {
+export function WidgetChooserAccordion({
+    entries
+}: {
+    entries: WidgetChooserNode[] | undefined;
+}) {
+    if (!entries) {
+        return null;
+    }
     return (
-        <div className="overflow-y-auto pb-2" style={{ maxHeight: size }}>
-            <Accordion className="relative" alwaysOpen={true}>
-                {entries.map((e: any) => (
-                    <AccordionItem key={e.key}>
-                        {({ open }: { open: boolean }) => (
-                            <>
-                                <AccordionTitle dataKey={e.key} title={e.title} open={open} />
-                                {createSubAccordion(e.content, 1)}
-                            </>
-                        )}
-                    </AccordionItem>
+        <div className="min-h-min w-full flex">
+            <Accordion className="relative w-full" alwaysOpen>
+                {entries.map(e => (
+                    <SubWidgetChooserAccordion
+                        key={e.key}
+                        entries={e}
+                        indent={8}
+                    />
                 ))}
             </Accordion>
         </div>
     );
-
 }
