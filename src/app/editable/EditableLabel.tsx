5172526be1d100433780ca0b280ca447ee288ff9
Drew Beamer <andrewmbeamer@gmail.com>
2024-07-09 11:38:53 -0400

    cleanup tabs, navbar

--- src/app/editable/EditableLabel.tsx
+++ src/app/editable/EditableLabel.tsx
@@ -1,51 +1,85 @@
-import { useEffect, useState, useRef, MutableRefObject, Dispatch, SetStateAction } from "react";
+import {
+  useEffect,
+  useState,
+  useRef,
+  MutableRefObject,
+  Dispatch,
+  SetStateAction,
+  useCallback,
+} from "react";
 import { useClickedOut } from "../tabs/hooks/useClickedOut";
 
+export function EditableLabel({
+  label,
+  setLabel,
+}: {
+  label: string;
+  setLabel: Dispatch<SetStateAction<string>>;
+}) {
+  const [editedLabel, setEditedLabel] = useState(label);
+  const editedRef: MutableRefObject<HTMLSpanElement | null> = useRef(null);
 
-export function EditableLabel({ label, setLabel }: { label: string, setLabel: Dispatch<SetStateAction<string>> }) {
-    const [editedLabel, setEditedLabel] = useState(label);
-    const editedRef: MutableRefObject<HTMLSpanElement | null> = useRef(null);
+  const [editing, setEditing] = useState(false);
+  const { componentRef, clickedOut } = useClickedOut();
 
-    const [editing, setEditing] = useState(false);
-    const { componentRef, clickedOut } = useClickedOut();
-
-    const updateEditLabel = (s: string) => {
-        if (editedRef.current) {
-            editedRef.current.textContent = s;
-        }
+  const updateEditLabel = (s: string) => {
+    if (editedRef.current) {
+      editedRef.current.textContent = s;
     }
+  };
 
-    useEffect(() => {
-        if (editing) {
-            clickedOut ? handleFinishedEdit() : updateEditLabel(label);
-        }
-    }, [clickedOut, editing])
-
-    const handleFinishedEdit = () => {
-        setEditing(false);
-
-        const newLabel = editedRef.current?.textContent!;
-
-        if (!newLabel || newLabel.trim().length < 1) {
-            setEditedLabel(label)
-        } else {
-            setLabel(newLabel);
-        }
+  const handleFinishedEdit = () => {
+    setEditing(false);
+    const newLabel = editedRef.current?.textContent;
+    if (!newLabel || newLabel.trim().length < 1) {
+      setEditedLabel(label);
+    } else {
+      setLabel(newLabel);
+      setEditedLabel(newLabel);
     }
+  };
 
-    return (
-        <div>
-            <span
-                ref={componentRef} onDoubleClick={() => setEditing(true)} onKeyDown={e => {
-                    if (e.key === "Enter") { handleFinishedEdit() }
-                }}>
-                <span className={`${editing ? "hidden" : ""}`}>{label}</span>
-                <span
-                    ref={editedRef}
-                    className={`${editing ? "" : "hidden"}`}
-                    suppressContentEditableWarning={true}
-                    contentEditable={editing}>{editedLabel}</span>
-            </span >
-        </div>
-    )
+  const updateEditLabelCallback = useCallback(updateEditLabel, []);
+  const handleFinishedEditCallback = useCallback(handleFinishedEdit, [
+    label,
+    setLabel,
+  ]);
+
+  useEffect(() => {
+    if (editing) {
+      clickedOut
+        ? handleFinishedEditCallback()
+        : updateEditLabelCallback(editedLabel);
+    }
+  }, [
+    editing,
+    clickedOut,
+    handleFinishedEditCallback,
+    updateEditLabelCallback,
+    editedLabel,
+  ]);
+
+  return (
+    <div>
+      <span
+        ref={componentRef}
+        onDoubleClick={() => setEditing(true)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleFinishedEdit();
+          }
+        }}
+      >
+        <span className={`${editing ? "hidden" : ""}`}>{label}</span>
+        <span
+          ref={editedRef}
+          className={`${editing ? "" : "hidden"}`}
+          suppressContentEditableWarning={true}
+          contentEditable={editing}
+        >
+          {editedLabel}
+        </span>
+      </span>
+    </div>
+  );
 }
--- src/store/tabStore.ts
+++ src/store/tabStore.ts
@@ -1,78 +1,94 @@
 import { create } from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 import { produce } from "immer";
-import { createLocalStorageProvider, generateId, getStorageEngine } from "./util";
+import { generateId, getStorageEngine } from "./util";
 type Tab = {
-    id: string,
-    name: string,
-    deleted: boolean
-}
+  id: string;
+  name: string;
+  deleted: boolean;
+};
 
 export type TabStore = {
-    tabs: Tab[],
-    activeTab: string,
-    actions: Actions
-}
+  tabs: Tab[];
+  activeTab: string;
+  actions: Actions;
+};
 
 export type Actions = {
-    addTab: () => void,
-    deleteTab: (id: string) => void,
-    setActiveTab: (id: string) => void,
-    renameTab: (id: string, name: string) => void
-}
+  addTab: () => void;
+  deleteTab: (id: string) => void;
+  setActiveTab: (id: string) => void;
+  renameTab: (id: string, name: string) => void;
+};
 
 const immerActions = {
-    addTab: (tabs: Tab[]) => produce(tabs, nextState => { nextState.push({ id: generateId(), name: "New Tab", deleted: false }) }),
-    deleteTab: (tabs: Tab[], setActiveTab: (id: string) => void, id: string) => produce(tabs, nextState => {
-        let index = 0;
-        const nt = nextState.filter((a, i) => {
-            if (a.id === id) {
-                index = i;
-                a.deleted = true;
-                return false;
-            }
-            return true;
-        });
-
-        let at;
-
-        if (index === -1) {
-            at = (nt[0].id);
-        } else if (index === nextState.length - 1) {
-            at = (nt[nt.length - 1].id)
-        } else {
-            at = nt[index].id;
-        }
-
-        setActiveTab(at);
+  addTab: (tabs: Tab[]) =>
+    produce(tabs, (nextState) => {
+      nextState.push({ id: generateId(), name: "New Tab", deleted: false });
     }),
-    renameTab: (tabs: Tab[], id: string, name: string) => produce(tabs, nextState => {
-        const n = nextState.find(z => z.id === id);
-        if (n) {
-            n.name = name;
+  deleteTab: (tabs: Tab[], setActiveTab: (id: string) => void, id: string) =>
+    produce(tabs, (nextState) => {
+      let index = 0;
+      const nt = nextState.filter((a, i) => {
+        if (a.id === id) {
+          index = i;
+          a.deleted = true;
+          return false;
         }
-    })
-}
+        return true;
+      });
+
+      let at;
+
+      if (index === -1) {
+        at = nt[0].id;
+      } else if (index === nextState.length - 1) {
+        at = nt[nt.length - 1].id;
+      } else {
+        at = nt[index].id;
+      }
+
+      setActiveTab(at);
+    }),
+  renameTab: (tabs: Tab[], id: string, name: string) =>
+    produce(tabs, (nextState) => {
+      const n = nextState.find((z) => z.id === id);
+      if (n) {
+        n.name = name;
+      }
+    }),
+};
 
 export const useTabStore = create<TabStore>()(
-    persist((set, get) => ({
-        tabs: [
-            { id: generateId(), name: "Autonomous", deleted: false },
-            { id: generateId(), name: "Teleop", deleted: false },
-        ],
-        activeTab: "first",
-        actions: {
-            setActiveTab: (id: string) => set({ activeTab: id }),
-            addTab: () => set({ tabs: immerActions.addTab(get().tabs) }),
-            deleteTab: (id: string) => set({ tabs: immerActions.deleteTab(get().tabs, get().actions.setActiveTab, id) }),
-            renameTab: (id: string, name: string) => set({ tabs: immerActions.renameTab(get().tabs, id, name) })
-        }
-    }), {
-        name: "tabStore",
-        storage: getStorageEngine(),
-        partialize: (state) => ({
-            tabs: state.tabs,
-            activeTab: state.activeTab
-        })
-    })
+  persist(
+    (set, get) => ({
+      tabs: [
+        { id: generateId(), name: "Autonomous", deleted: false },
+        { id: generateId(), name: "Teleop", deleted: false },
+      ],
+      activeTab: "first",
+      actions: {
+        setActiveTab: (id: string) => set({ activeTab: id }),
+        addTab: () => set({ tabs: immerActions.addTab(get().tabs) }),
+        deleteTab: (id: string) =>
+          set({
+            tabs: immerActions.deleteTab(
+              get().tabs,
+              get().actions.setActiveTab,
+              id
+            ),
+          }),
+        renameTab: (id: string, name: string) =>
+          set({ tabs: immerActions.renameTab(get().tabs, id, name) }),
+      },
+    }),
+    {
+      name: "tabStore",
+      storage: getStorageEngine(),
+      partialize: (state) => ({
+        tabs: state.tabs,
+        activeTab: state.activeTab,
+      }),
+    }
+  )
 );
